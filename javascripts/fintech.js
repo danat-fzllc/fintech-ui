dc43f6762b9e2a5a033ceb32eed60f27681b8af5
Rakesh <merakeshvk@gmail.com>
2017-01-30 12:47:30 +0400

    HTML/CSS Updates.
    Add the class "active" for the Discover button in the Question when user change any of the question filter.
    
    - Q4_2_1_event_effect_aggregate.html
    
      1, Company name moved to outside the "content" div.

--- javascripts/fintech.js
+++ javascripts/fintech.js
@@ -1,3 +1,8 @@
+function Expand(obj){
+      if (!obj.savesize) obj.savesize=obj.size;
+      obj.size=Math.max(obj.savesize,obj.value.length);
+     }
+     
 $(window).on('scroll',function() {    
     var scroll = $(window).scrollTop();
 
@@ -102,3 +107,4 @@ $(resize).resizable({
 
 });
 });
+
