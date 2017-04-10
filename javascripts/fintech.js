101642c6e4d6a753a9c280d20c328ba1fe3ed27d
Sabin Antony <sabinantony01@gmail.com>
2017-04-10 12:18:35 +0400

    Mobile - CSS

--- javascripts/fintech.js
+++ javascripts/fintech.js
@@ -1,9 +1,9 @@
-function Expand(obj){
-      if (!obj.savesize) obj.savesize=obj.size;
-      obj.size=Math.max(obj.savesize,obj.value.length);
-     }
+function Expand(obj) {
+    if (!obj.savesize) obj.savesize = obj.size;
+    obj.size = Math.max(obj.savesize, obj.value.length);
+}
 
-$(window).on('scroll',function() {    
+$(window).on('scroll', function() {
     var scroll = $(window).scrollTop();
 
     if (scroll >= 200) {
@@ -15,49 +15,46 @@ $(window).on('scroll',function() {
 //semantic modules
 //dropdown
 $('.ui.dropdown')
-  .dropdown({
-    allowCategorySelection: true
-  });
+    .dropdown({
+        allowCategorySelection: true
+    });
 $('.ui.progress').progress({
- showActivity:false
+    showActivity: false
 });
 
 // clear the generated semantic-ui menu
 var $menu = $('<div/>').addClass('menu');
-$('.optgroups').each(function(){
-   //debugger
-   var parentthis=$(this);
-   $(this).find('optgroup').each(function (index, element) {
-      $menu.append('<div class="header ui horizontal divider">' + element.label + '</div>')
-      $(element).children().each(function(i, e){
-          $menu.append('<div class="item" data-value="' + e.value + '">' + e.innerHTML + '</div>');
-      })
-   })
-   $(this).find('.menu').html($menu.html());
-   $menu="";
-   $menu = $('<div/>').addClass('menu');
+$('.optgroups').each(function() {
+    //debugger
+    var parentthis = $(this);
+    $(this).find('optgroup').each(function(index, element) {
+        $menu.append('<div class="header ui horizontal divider">' + element.label + '</div>')
+        $(element).children().each(function(i, e) {
+            $menu.append('<div class="item" data-value="' + e.value + '">' + e.innerHTML + '</div>');
+        })
+    })
+    $(this).find('.menu').html($menu.html());
+    $menu = "";
+    $menu = $('<div/>').addClass('menu');
 });
 
 
 
 
 $('.ui.checkbox')
-  .checkbox()
-;
+    .checkbox();
 $('.ui.accordion')
-  .accordion()
-;
+    .accordion();
 
 $('.button.date-period')
-  .popup({
-        inline : true,
-        position   : 'top left'
-  })
-;
+    .popup({
+        inline: true,
+        position: 'top left'
+    });
 //Share 
 $('.button.share').click(function() {
-        $('.button.share .list').toggle(300);
-    });
+    $('.button.share .list').toggle(300);
+});
 $(document).mouseup(function(e) {
     var container = $(".button.share .list");
     if (!container.is(e.target) // if the target of the click isn't the container...
@@ -72,39 +69,59 @@ $(".sidebar-toggler-arrow").click(function() {
     $(".ui.content.container").toggleClass("no-side-bar");
 });
 
-
+$('.tabs-home .item').tab({
+    history: false
+});
 
 //resizing pannel script
 //<![CDATA[
-$(window).load(function(){
-$(document).ready(function() {
+$(window).load(function() {
+    $(document).ready(function() {
+
+        $(".main-nav-button").click(function() {
+            $("body").addClass("menu-open");
+
+            var height = $(".main-nav .holder").height();
+            $(".main-nav .holder").css({
+                height: height
+            });
+            $(".main-nav .holder").addClass("menu-holder");
+        });
+        $(".main-nav-close").click(function() {
+            $("body").removeClass("menu-open");
+        });
 
 
-if ($(window).width() > 1000) {   
-var resize= $(".resize-left-column");
-var containerWidth = $("#container").width();
-   
-$(resize).resizable({
-     handles: 'e',
-     maxWidth: 350,
-     minWidth: 250,
-     resize: function(event, ui){
-         var currentWidth = ui.size.width;
-         
-         // this accounts for padding in the panels + 
-         // borders, you could calculate this using jQuery
-         var padding = 12; 
-         
-         // this accounts for some lag in the ui.size value, if you take this away 
-         // you'll get some instable behaviour
-         $(this).width(currentWidth);
-         
-         // set the content panel width
-         $(".resize-right-column").width(containerWidth - currentWidth - padding);            
-     }
+
+        if ($(window).width() > 1000) {
+            var resize = $(".resize-left-column");
+            var containerWidth = $("#container").width();
+
+            $(resize).resizable({
+                handles: 'e',
+                maxWidth: 350,
+                minWidth: 250,
+                resize: function(event, ui) {
+                    var currentWidth = ui.size.width;
+
+                    // this accounts for padding in the panels + 
+                    // borders, you could calculate this using jQuery
+                    var padding = 12;
+
+                    // this accounts for some lag in the ui.size value, if you take this away 
+                    // you'll get some instable behaviour
+                    $(this).width(currentWidth);
+
+                    // set the content panel width
+                    $(".resize-right-column").width(containerWidth - currentWidth - padding);
+                }
+            });
+            $(".grid.home .ui.tab").addClass("column").removeClass("segment ui tab");
+        } else {
+            var TopHeight = $(".fintech .top").height() + 8;
+            $(".main-container").css({
+                'padding-top': TopHeight
+            });
+        }
+    });
 });
-}
-
-});
-});
-
